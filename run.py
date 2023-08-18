545b5e2398549a7b01a0a6a7507d727268e976f5
Soderberg <Christoffer.Soderberg@ons.gov.uk>
2023-08-18 16:13:55 +0100

    Added gitignore and comments to run.py

--- run.py
+++ run.py
@@ -3,18 +3,24 @@ import functions
 
 def main():
 
+    # Generate two dataframes: a, b
     df_a = functions.random_dataframe(5,3)
-    df_b = functions.random_dataframe(3,2)
+    df_b = functions.random_dataframe(3,8)
 
+    # Display results
     print(df_a, '\n')
     print(df_b, '\n')
 
+    # Create product a*b
     product_ab = functions.dataframe_multiply(df_a, df_b)
 
+    # View product
     print(product_ab, '\n')
     
+    # Create identity matrix compatible with a*b
     id_matrix_ab = functions.identity_function(product_ab.shape[1])
 
+    # Verify a*b*I = a*b
     print(id_matrix_ab, '\n')
 
     print(functions.dataframe_multiply(product_ab, id_matrix_ab))
